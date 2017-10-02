39bb2fed030ac6e24e5a45048bab22fa8ae5541a
Sarah Ball <hi@sarahb.co>
2017-10-02 19:33:32 -0400

    updates to locations selector

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -2,6 +2,7 @@
 //= require jquery
 //= require jquery-ui
 //=require jquery-ui/widgets/accordion
+//=require jquery-ui/widgets/selectmenu
 //= require jquery.matchHeight
 
 $( function() {
@@ -20,4 +21,28 @@ $(function() {
     target: null,
     remove: false
   });
+});
+
+$(function() {
+  $("#wherefrom").selectmenu({
+  })
+});
+
+$(function() {
+  $("#wheregoing").selectmenu({
+    change: function(event, data){ 
+      if (data.item.value == "going1") { 
+        $("body").css("background", "url(/images/bg-london.jpg) no-repeat center center fixed");
+      }
+      else if (data.item.value == "going2") {
+        $("body").css("background", "url(/images/bg-chiangmai.jpg) no-repeat center center fixed");
+      }
+      else if (data.item.value == "going3") {
+        $("body").css("background", "url(/images/bg-berlin.jpg) no-repeat center center fixed");
+      }
+      else if (data.item.value == "going4") {
+        $("body").css("background", "url(/images/bg-lisbon.jpg) no-repeat center center fixed");
+      }
+    }
+  })
 });
