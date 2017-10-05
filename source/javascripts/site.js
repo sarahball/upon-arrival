a005469e04006a6642fbad8e41e7eb26382fa87d
Sarah Ball <hi@sarahb.co>
2017-10-05 11:56:22 -0400

    locations selector in progress

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -24,11 +24,8 @@ $(function() {
 });
 
 $(function() {
-  $("#wherefrom").selectmenu({
-  })
-});
-
-$(function() {
+  $("#wherefrom").selectmenu();
+  
   $("#wheregoing").selectmenu({
     change: function(event, data){ 
       if (data.item.value == "going1") { 
@@ -46,3 +43,31 @@ $(function() {
     }
   })
 });
+
+
+
+/*$( function() {
+  $.widget( "custom.iconselectmenu", $.ui.selectmenu, {
+    _renderItem: function( ul, item ) {
+      var li = $( "<li>" ),
+        wrapper = $( "<div>", { text: item.label } );
+
+      if ( item.disabled ) {
+        li.addClass( "ui-state-disabled" );
+      }
+
+      $( "<span>", {
+        style: item.element.attr( "data-style" ),
+        "class": "ui-icon " + item.element.attr( "data-class" )
+      })
+        .appendTo( wrapper );
+
+      return li.append( wrapper ).appendTo( ul );
+    }
+  });
+
+  $( "#wherefrom" )
+    .iconselectmenu()
+    .iconselectmenu( "menuWidget" )
+      .addClass( "ui-menu-icons customicons" );
+} );*/
