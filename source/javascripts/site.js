0980d09a286c9edba5bb3fa01b22f5ca975676d1
Mike Rogers <me@mikerogers.io>
2017-10-11 10:34:41 +0200

    Overriding the body class instead of adding new when location changes

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -30,36 +30,8 @@ $(function() {
   
   $("#wheregoing").selectmenu({
     change: function(event, data){ 
-      if (data.item.value == "going1") { 
-        document.body.classList.add("locations-london");
-      }
-      else if (data.item.value == "going2") {
-        document.body.classList.add("locations-chiangmai");
-      }
-      else if (data.item.value == "going3") {
-        document.body.classList.add("locations-berlin");
-      }
-      else if (data.item.value == "going4") {
-       document.body.classList.add("locations-lisbon");
-      }
-      else if (data.item.value == "going5") {
-       document.body.classList.add("locations-newyorkcity");
-      }
-      else if (data.item.value == "going6") {
-       document.body.classList.add("locations-barcelona");
-      }
-      else if (data.item.value == "going7") {
-       document.body.classList.add("locations-bangkok");
-      }
-      else if (data.item.value == "going8") {
-       document.body.classList.add("locations-buenosaires");
-      }
-      else if (data.item.value == "going9") {
-       document.body.classList.add("locations-amsterdam");
-      }
-      else if (data.item.value == "going10") {
-       document.body.classList.add("locations-budapest");
-      }
+      var labelClassName = data.item.label.toLowerCase().replace(' ', '');
+      $('body').attr('class', 'locations locations-'+labelClassName);
     }
   })
 
