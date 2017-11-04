be05816df622cb9e64cc215ae87c6830fd16b0bd
Mike Rogers <me@mikerogers.io>
2017-11-04 13:14:35 +0000

    Adding in Dark Sky API for showing current weather

---
+++ helpers/weather_helper.rb
@@ -0,0 +1,34 @@
+module WeatherHelper
+  def weather(destination)
+    @weather ||= {}
+    @weather[destination.slug] ||= get_weather(destination)
+  end
+
+  def get_weather(destination)
+    app.cache "dark-sky/#{destination.latitude_dec},#{destination.longitude_dec}" do
+      uri = URI("https://api.darksky.net/forecast/#{ENV['DARKSKY_API_KEY']}/#{destination.latitude_dec},#{destination.longitude_dec}?units=si&exclude=minutely,currently,flags")
+      response = Net::HTTP.get(uri)
+      JSON.parse(response)
+    end
+  end
+
+  def weather_temperature_high(destination)
+    "#{weather(destination)['daily']['data'].first['temperatureHigh']} °c"
+  end
+
+  def weather_temperature_low(destination)
+    "#{weather(destination)['daily']['data'].first['temperatureLow']} °c"
+  end
+
+  def weather_today_summary(destination)
+    weather(destination)['hourly']['data'].first['summary']
+  end
+
+  def weather_today_icon(destination)
+    weather(destination)['daily']['icon']
+  end
+
+  def weather_typical(destination)
+    'Temperate. Moderated by prevailing southwest winds over the North Atlantic Current. More than one-half of the days are overcast.'
+  end
+end
