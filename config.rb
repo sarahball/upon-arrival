a10b68482b8484ccf49035956061bd19ad95f580
Mike Rogers <me@mikerogers.io>
2017-11-15 19:44:15 +0000

    Making minfier less aggressive

--- config.rb
+++ config.rb
@@ -72,22 +72,22 @@ configure :build do
   activate :minify_css
   activate :minify_javascript
   activate :asset_hash, ignore: %r{^images/.*}
-   activate :minify_html do |html|
+  activate :minify_html do |html|
     html.remove_multi_spaces        = true   # Remove multiple spaces
     html.remove_comments            = true   # Remove comments
     html.remove_intertag_spaces     = false  # Remove inter-tag spaces
     html.remove_quotes              = false  # Remove quotes
     html.simple_doctype             = false  # Use simple doctype
     html.remove_script_attributes   = false  # Remove script attributes
-    html.remove_style_attributes    = true   # Remove style attributes
-    html.remove_link_attributes     = true   # Remove link attributes
+    html.remove_style_attributes    = false  # Remove style attributes
+    html.remove_link_attributes     = false  # Remove link attributes
     html.remove_form_attributes     = false  # Remove form attributes
     html.remove_input_attributes    = false  # Remove input attributes
-    html.remove_javascript_protocol = true   # Remove JS protocol
+    html.remove_javascript_protocol = false  # Remove JS protocol
     html.remove_http_protocol       = false  # Remove HTTP protocol
     html.remove_https_protocol      = false  # Remove HTTPS protocol
     html.preserve_line_breaks       = false  # Preserve line breaks
-    html.simple_boolean_attributes  = true   # Use simple boolean attributes
+    html.simple_boolean_attributes  = false  # Use simple boolean attributes
     html.preserve_patterns          = nil    # Patterns to preserve
   end
   activate :gzip
