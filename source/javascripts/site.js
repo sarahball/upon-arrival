7476e416eed5f1d170aaf98f83117e6035e7049e
Sarah Ball <hi@sarahb.co>
2017-11-16 13:08:21 -0800

    adding CSS grid

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,18 +1,22 @@
 // This is where it all goes :)
 //= require jquery
-//= require jquery-ui
 //=require jquery-ui/widgets/accordion
 //=require jquery-ui/widgets/selectmenu
+//=require jquery-ui/widgets/tabs
 //= require jquery.matchHeight
 
 $( function() {
-  $( "#accordion" ).accordion({
-  	collapsible: true,
-  	active: false,
-  	heightStyle: "content",
-  	header: ".item-title"
+  $("#tabs li a").click(function (e) {
+      $(this).closest('#tabs').addClass('menu-open'); // I also tried .parent().addClass
   });
 } );
+
+$( function() {
+    $( "#tabs" ).tabs({
+      collapsible: true,
+      active: false
+    });
+  } );
 
 $(function() {
 	$('.item-title').matchHeight({
