8abd72c7f46e31df0d9602885d8de0e413905378
Mike Rogers <me@mikerogers.io>
2017-11-16 12:29:36 +0000

    Add climate information from Factbook
    Closes #22

--- helpers/trips_helper.rb
+++ helpers/trips_helper.rb
@@ -16,8 +16,17 @@ module TripsHelper
         latitude_dec: country_data.latitude_dec,
         longitude_dec: country_data.longitude_dec,
         currency: country_data.currency,
-        languages: country_data.languages
+        languages: country_data.languages,
+        climate: get_climate(destination_data.factbook_country_code || destination_data.country)
       })
     end
+  end
+
+  def get_climate(country_code)
+    climate = app.cache "factbook/climate/#{country_code}" do
+      Factbook::Page.new(country_code).climate
+    end
+
+    "#{climate.capitalize}."
   end
 end
