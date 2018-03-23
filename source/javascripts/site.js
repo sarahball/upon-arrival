abcfb92c1f253f631ba72484bf0cdf3ab882c4b0
Sarah Ball <hi@sarahb.co>
2018-03-22 17:52:58 -0700

    puma drama

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -12,6 +12,15 @@
   });
 } );*/
 
+/* Applying the "open" class when you click the Menu button, which triggers the mobile menu to open/close; Toggle Menu/Close menu text. */
+$(document).ready(function() {
+  $('.mobile-button a').click(function(e) {
+  $(this).parent().parent().toggleClass('open');
+  $(this).html($(this).html() == 'Close Menu' ? 'Menu' : 'Close Menu');
+    e.preventDefault();
+  });
+});
+
 $( function() {
     $( "#tabs" ).tabs({
       collapsible: true,
