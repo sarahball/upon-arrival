6a1423b74331736189dedf4c86e3434db5293ebf
Mike Rogers <me@mikerogers.io>
2018-06-16 20:21:41 +0100

    Darn it heroku - I want to demo the login

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,1 @@
-if ENV['MEMCACHEDCLOUD_SERVERS']
-  Rails.application.config.session_store ActionDispatch::Session::CacheStore, cache: ActiveSupport::Cache::MemCacheStore.new((ENV['MEMCACHEDCLOUD_SERVERS'] || '').split(','), { username: ENV['MEMCACHEDCLOUD_USERNAME'], password: ENV['MEMCACHEDCLOUD_PASSWORD'], failover: true, socket_timeout: 1.5, socket_failure_delay: 0.2, down_retry_delay: 60 })
-end
+Rails.application.config.session_store :cookie_store, key: '_upon_arrival' # , domain: :all
