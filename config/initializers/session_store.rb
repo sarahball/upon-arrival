fa68bc221aa525a785ad709bbb0dd94106984697
Mike Rogers <me@mikerogers.io>
2018-06-16 20:19:18 +0100

    Using memcache for sessions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
-# Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :redis_store
+if ENV['MEMCACHEDCLOUD_SERVERS']
+  Rails.application.config.session_store ActionDispatch::Session::CacheStore, cache: ActiveSupport::Cache::MemCacheStore.new((ENV['MEMCACHEDCLOUD_SERVERS'] || '').split(','), { username: ENV['MEMCACHEDCLOUD_USERNAME'], password: ENV['MEMCACHEDCLOUD_PASSWORD'], failover: true, socket_timeout: 1.5, socket_failure_delay: 0.2, down_retry_delay: 60 })
+end
