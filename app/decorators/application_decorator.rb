d7e59dbf51aedc72c2d88d515662949a86ad77c4
Mike Rogers <me@mikerogers.io>
2018-06-19 22:37:31 +0100

    Allow h4 and h5

--- app/decorators/application_decorator.rb
+++ app/decorators/application_decorator.rb
@@ -6,6 +6,6 @@ class ApplicationDecorator < Draper::Decorator
   end
 
   def markdown_sanitize(body)
-    h.sanitize(Kramdown::Document.new(body).to_html, tags: %w(p br a ul ol li strong em h2 h3 table tr td))
+    h.sanitize(Kramdown::Document.new(body).to_html, tags: %w(p br a ul ol li strong em h2 h3 h4 h5 table tr td))
   end
 end
